befcdbfa45a75b40cafe1206dae155c2723fd4a3
Ralph-Wang <ralph_ld@aliyun.com>
2014-02-25 14:21:57 +0800

    处理边栏分类

---
+++ plugins/category_list.rb
@@ -0,0 +1,17 @@
+module Jekyll
+    class CategoryListTag < Liquid::Tag
+        def render(context)
+            html = ""
+            categories = context.registers[:site].categories.keys
+            categories.sort.each do |category|
+                posts_in_category = context.registers[:site].categories[category].size
+                category_dir = context.registers[:site].config['category_dir']
+                category_url = File.join(category_dir, category.to_url)
+                html << "<a class='list-group-item' href='/#{category_url}/'>#{category} <span class='badge'>#{posts_in_category}</span></a>\n"
+            end
+            html
+        end
+    end
+end
+
+Liquid::Template.register_tag('category_list', Jekyll::CategoryListTag)
